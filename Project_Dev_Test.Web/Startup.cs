8e8943dbad4da26f7a539cabdcf4a45f54702d5f
José Henrique Ivanchechen <jose.henrique.ivan@gmail.com>
2023-06-29 16:50:23 -0300

    💰swaaaagg💸💸🤑🤑🤑🤑💸💲💲

--- Project_Dev_Test.Web/Startup.cs
+++ Project_Dev_Test.Web/Startup.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.OpenApi.Models;
-using System;
-using System.Reflection;
-using Microsoft.AspNetCore.Http.Features;
-using Project_Dev_Test.Web.Service;
+﻿using Microsoft.AspNetCore.Http.Features;
 using Project_Dev_Test.Web.Repository;
+using Project_Dev_Test.Web.Service;
 
 namespace Project_Dev_Test.Web
 {
@@ -45,43 +42,43 @@ namespace Project_Dev_Test.Web
             services.AddControllersWithViews();
             //.AddNewtonsoftJson();
 
-            services.AddRazorPages();
+            //services.AddRazorPages();
 
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
+            //services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.EnvironmentName == "Development")
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            //if (env.EnvironmentName == "Development")
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
             app.UseRouting();
 
             DataRepository.InitializeDatabase();
             Helpers.MatrixModel.Initialize();
 
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseStaticFiles();
+            //app.UseCookiePolicy();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+            //// Enable middleware to serve generated Swagger as a JSON endpoint.
+            //app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1.2.1");
-            });
+            //// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            //app.UseSwaggerUI(c => {
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1.2.1");
+            //});
             
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapRazorPages();
+                //endpoints.MapRazorPages();
             });
         }
 
